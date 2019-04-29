d0f8c9b7044e166de2bfe72ca793faa1026bec63
NortonS1 <sam.norton11@hotmail.co.uk>
2019-04-30 10:50:49 +1200

    commented

--- brick plots reborn.R
+++ brick plots reborn.R
@@ -7,23 +7,25 @@ library(viridis)
 library(plotrix)
 #############################################################################
 ##############################DEFINE LOCATIONS###############################
+#Generate new plot
 plot.new()
 op <- par(mfrow = c(2, 2))
-
-cor_mat_file <- file.choose()
+#Load in data
+cor_mat_file <- file.choose() # importing correlation matrix created elsewhere - to update. 
 data_set <<- read.csv(cor_mat_file)
 data_set = data_set[, 2:length(data_set[1, ])] 
+
 range01 <- function(x) {
   (x - min(x)) / (max(x) - min(x))
-}
+} #defining scale function
 
-data_set = apply(data_set, 2, range01)
+data_set = apply(data_set, 2, range01) # applying scale function
 data_set = as.data.frame(data_set)
 k.max <- 30
 data <- data_set
 wss <- sapply(1:k.max, function(k) {
   kmeans(data, k)$tot.withinss
-})
+}) #performing iterative Kmeans
 
 plot(
   1:k.max,
@@ -33,31 +35,30 @@ plot(
   frame = FALSE,
   xlab = "Number of clusters K",
   ylab = "Total within-clusters sum of squares"
-)
-k = 3
-km = kmeans(data, k)
+) # plotting elbow plot validation
+k = 3 #manually assign K based on elbow plot
+km = kmeans(data, k) #re-run Kmeans with selected K
 cluster_length = length(km$cluster[which(km$cluster == 1)])
-# USE km$cluster to define colours
+
+# use km$cluster to define colours
 broad_colours = km$cluster*100
-broad_colours = as.data.frame(broad_colours)
+broad_colours = as.data.frame(broad_colours) #defining clusters as a colour
 
 cluster_types = unique(broad_colours)
 number_clusters = length(cluster_types$broad_colours)
-palette_2 = magma((100*(number_clusters+1)))
+palette_2 = magma((100*(number_clusters+1))) #defining palette
 
 for(cluster_group in cluster_types$broad_colours) {
   iteration = 1
   name = cluster_group
   for (i in 1:length(broad_colours$broad_colours)) {
-    # cluster_length = length(broad_colours$broad_colours[which(broad_colours$broad_colours ==broad_colours$broad_colours[i])])
     if (broad_colours$broad_colours[i] == name) {
-      broad_colours$broad_colours[i] <<- broad_colours$broad_colours[i] + (as.integer(60 / length(markers)) * iteration) #FIX THIS LINE
+      broad_colours$broad_colours[i] <<- broad_colours$broad_colours[i] + (as.integer(60 / length(markers)) * iteration) 
       iteration = iteration + 1
     }
   }
-}
-range(x)
-pc = (princomp(data))
+} #applying colour range within a cluster
+pc = (princomp(data)) #performing principle component analysis
 plot(pc, type = "l")
 x = pc$scores[, 1]
 y = pc$scores[, 2]
@@ -70,29 +71,29 @@ plot(
   ylab = "",
   axes = FALSE,
   cex = 2
-)
+) #plotting PCA variation curve
 
 print("Markers Clustered")
 
 
 ############################################################################
 ##############################CREATE BOXES##################################
-
+#Generate new plot
 
 plot.new()
 op <- par(mfrow = c(2, 2))
-
+#load in phenotype data
 phe_file <- file.choose()
-
 all_data <- read.csv(phe_file)
 
 print("cluster data assigned")
-qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual', ]
-palette = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
+#scaling data
 scaled_data <- apply(all_data[,-1], 2, scale)
 scaled_data = scaled_data
 markers <- colnames(scaled_data)
+
+#defining functions
 make_brick = function(edge,
                       place_brick = FALSE,
                       i = 1) {
@@ -128,6 +129,8 @@ place_brick <- function(a, b, width, height, cols) {
 
 print("place brick function defined")
 
+#generating boxes for each cluster and labelling
+
 for (cluster in 1:length(scaled_data[, 1])) {
   print(paste("plotting cluster", cluster, sep = " "))
   values = (scaled_data[cluster,])
@@ -137,20 +140,13 @@ for (cluster in 1:length(scaled_data[, 1])) {
     }
   }
   values = as.data.frame(values)
-  print("1")
   colours = seq(1, length(scaled_data[1,]), 1)
-  print("2")
   colours = as.data.frame(colours)
-  print("3")
   rownames(colours) = rownames(values)
-  print("4")
   broad_colours = as.data.frame(broad_colours)
-  print("5")
   rownames(broad_colours) = rownames(values)
-  print("6")
   values = cbind(values, colours, x, y,broad_colours)
-  print("7")
-  
+
   positions <<- matrix(data = NA, length(scaled_data[1,]), 4)
   positions[1,] = c(0, 0, 0, 0)
   test_box <<- matrix(data = NA, 1, 4)
